f91eded6776b53003f8c7956bc694b73c347556d
brayan <brayan.mail.tester@gmail.com>
2023-12-21 17:03:17 -0600

    chore: appjsx modifed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className='text-6xl'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-6xl font-semibold text-emerald-700 text-center py-10'>CRUD WHIT REDUX</h1>
+      
     </>
   )
 }
